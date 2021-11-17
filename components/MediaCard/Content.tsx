cf62503f63c0904b361df860faa5e1d9de4bcea7
o.kovalchuk <mail.to.kovalchuk@gmail.com>
2021-11-17 17:17:37 +0200

    fix/pre-img

--- components/MediaCard/Content.tsx
+++ components/MediaCard/Content.tsx
@@ -5,10 +5,10 @@ import {
   CircularProgress,
   Skeleton,
   Typography,
-} from "@mui/material";
-import MediaCardActivityValues from "./ActivityValues";
-import HoverVideoPlayer from "react-hover-video-player";
-import Image from "next/image";
+} from '@mui/material';
+import MediaCardActivityValues from './ActivityValues';
+import HoverVideoPlayer from 'react-hover-video-player';
+import Image from 'next/image';
 // TODO
 interface IProps {
   loading: boolean;
@@ -21,7 +21,7 @@ interface IProps {
   commentCount: number;
 }
 
-const MediaCardContent: React.FC<IProps> = (props) => {
+const MediaCardContent: React.FC<IProps> = props => {
   const {
     loading,
     videoUrl,
@@ -37,16 +37,12 @@ const MediaCardContent: React.FC<IProps> = (props) => {
     <Box>
       <Box
         sx={{
-          display: "flex",
-          alignItems: "end",
+          display: 'flex',
+          alignItems: 'end',
         }}
       >
         {loading ? (
-          <Skeleton
-            sx={{ height: 190 }}
-            animation="wave"
-            variant="rectangular"
-          />
+          <Skeleton sx={{ height: 190 }} animation="wave" variant="rectangular" />
         ) : (
           <HoverVideoPlayer
             videoSrc={videoUrl}
@@ -60,23 +56,24 @@ const MediaCardContent: React.FC<IProps> = (props) => {
               //   // placeholder="blur"
               //   layout="fill"
               // />
-              <Image
+              // eslint-disable-next-line @next/next/no-img-element
+              <img
                 alt=""
                 src={cover}
                 // src="/tiktok-art.jpg"
-                blurDataURL="/tiktok-logo.webp"
+                // blurDataURL="/tiktok-logo.webp"
                 // placeholder="blur"
-                layout="fill"
+                objectFit="contain"
               />
             }
             loadingOverlay={
               <Box
                 sx={{
-                  height: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
+                  height: '100%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
                 }}
               >
                 <CircularProgress />
@@ -95,11 +92,7 @@ const MediaCardContent: React.FC<IProps> = (props) => {
       <CardContent>
         {loading ? (
           <>
-            <Skeleton
-              animation="wave"
-              height={10}
-              style={{ marginBottom: 6 }}
-            />
+            <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
             <Skeleton animation="wave" height={10} width="80%" />
           </>
         ) : (
@@ -112,17 +105,17 @@ const MediaCardContent: React.FC<IProps> = (props) => {
         {loading ? (
           <Skeleton animation="wave" height={10} width="40%" />
         ) : (
-          hashtags.map((item) => (
+          hashtags.map(item => (
             <Box
               key={item.id}
               component="div"
               sx={{
-                typography: "subtitle2",
-                display: "inline-block",
+                typography: 'subtitle2',
+                display: 'inline-block',
                 // p: 1,
                 m: 0.2,
-                bgcolor: "background.paper",
-                cursor: "pointer",
+                bgcolor: 'background.paper',
+                cursor: 'pointer',
               }}
             >
               #{item.name}
